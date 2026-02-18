599ec9c741c59477f853a134cfd2c7e54e3ef79c
Kevin Santiago Pinto <kelopez@seedem.co>
2026-02-18 15:35:53 -0500

    prueba

--- build.sh
+++ build.sh
@@ -6,7 +6,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-
-# Limpiar y cargar datos de prueba
-python scripts/render_limpiar_bd.py
-python scripts/render_datos_prueba.py
